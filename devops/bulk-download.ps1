0633252b597146ecd7bf7feb52468efe3ca9dbdd
Glenn <glenn.ferrie@gmail.com>
2025-03-13 23:21:48 -0400

    many changes

---
+++ devops/bulk-download.ps1
@@ -0,0 +1,15 @@
+﻿$url = "https://unfounded.neocities.org/"
+$local_dir = "C:\_src\unfounded-site\"
+$files = "images/broadside_arena.jpg", "images/BSIDE_LARGE_GRID.png", 
+    "images/bubble_kitchen.jpg", "images/Janes_house.jpg", "images/logo.png", 
+    "images/prota_checkpoint.jpg", "images/prota_hq.jpg",
+    "images/sharecar.jpg", "images/slum_towers.jpg", "neocities.png"
+    , "rpg-style.css", "not_found.html", "favicon.ico", "broadside-rpg.htm"
+
+foreach($file in $files) {
+
+    $output = $local_dir + ($file.Replace("/", "\"))
+    Invoke-WebRequest -UseBasicParsing -Uri ($url + $file) -OutFile $output
+}
+
+Write-Host "Done"
---
+++ scripts/cryptotools.js
@@ -0,0 +1,111 @@
+var crypt = {
+	uuid: function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+            .replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0,
+                    v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+    },
+	currentChainId: "0x0",
+	chainName: function (id) {
+        if (!id) {
+            id = window.ethereum.chainId;
+            mmx.currentChainId = window.ethereum.chainId;
+        }
+        if (id == "0x0") { return "Disconnected"; }
+        if (id == "0x1") { return "Ethereum (mainnet)"; }
+        if (id == "0xaa36a7") { return "Sepolia (test)"; }
+        if (id == "0x89") { return "Polygon"; }
+        if (id == "0x38") { return "BNB Smart Chain"; }
+        if (id == "0x2105") { return "Base"; }
+        if (id == "0x13882") { return "Polygon (amoy/test)"; }
+        if (id == "0x14a34") { return "base-sepolia"; }
+        return id;
+    },
+	chainHandle: function (id) {
+        if (!id) {
+            id = window.ethereum.chainId;
+            mmx.currentChainId = window.ethereum.chainId;
+        }
+        if (id == "0x0") { return "unk"; }
+        if (id == "0x1") { return "eth"; }
+        if (id == "0xaa36a7") { return "sepolia"; }
+        if (id == "0x38") { return "bnb"; }
+        if (id == "0x89") { return "pol"; }
+        if (id == "0x2105") { return "base"; }
+        if (id == "0x13882") { return "pol-amoy"; }
+        if (id == "0x14a34") { return "base-sepolia"; }
+        return id;
+    },
+	maskedAddress: function (addr) {
+        if (addr) {
+            let len = addr.length;
+            let stubLeft = addr.substring(0, 7);
+            let stubRight = addr.substring(len - 5);
+            return `${stubLeft}....${stubRight}`;
+        }
+        return addr;
+    },
+    chains: {
+        Ethereum: "0x1", Polygon: "0x89", PolygonAmoy: "0x13882", Base: "0x2105", BaseSepolia: "0x14a34", Sepolia: "0xaa36a7", ShapeSepolia: ""
+    },
+    testChains: {
+        BaseSepolia: "0x14a34", Sepolia: "0xaa36a7", PolygonAmoy: "0x13882"
+    },
+    isTestChain: function (ch) {
+        let res = false;
+
+        let testChainList = [mmx.testChains.BaseSepolia, mmx.testChains.PolygonAmoy, mmx.testChains.Sepolia];
+        res = testChainList.indexOf(ch) > -1;
+        return res;
+    },
+    getContractRef: function (ca, abi, callback) {
+        //console.log({ getContract: { ca, abi } })
+        let provider = new ethers.BrowserProvider(window.ethereum);
+        provider.getSigner().then(signer => {
+            let contract = new ethers.Contract(ca, abi, signer);
+            callback(contract);
+        });
+    },
+	subscribeToChainEvents: function (acctChanged, chainChanged) {
+        window.ethereum.on("accountsChanged",acctChanged);
+        window.ethereum.on("chainChanged",chainChanged);
+    },
+	requestWalletConnect: function (onSuccess, onFail) {
+		if (window.ethereum) {
+            window.ethereum.request({ method: 'eth_requestAccounts' })
+                .then(accounts => {
+                  
+                    if (accounts.length === 0) {
+                        if (onFail) {
+							onFail()
+						}
+                    } else {
+                        if (onSuccess) {
+							onSuccess(accounts);
+						}
+                    }
+                });
+        } else {
+            alert('Please install a cryptowallet browser plugin.');
+        }		
+	},
+	testWalletConnection: function (onSuccess, onFail) {
+		if (window.ethereum) {
+			window.ethereum.request({ method: 'eth_accounts' }).then(accounts => {
+				if (accounts.length === 0) {	
+					if (onFail) {
+						onFail();
+					}
+				} else {
+					if (onSuccess) {
+						onSuccess(accounts);
+					}
+				}
+			});
+		} else {
+			alert('Please install a cryptowallet browser plugin.');
+		}
+	}
+}
